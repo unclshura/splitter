fd75af7f99764efb6166bb3095af054c4625a179
unclshura <unclshura@gmail.com>
2026-05-27 09:55:01 +0100

    App icon added

--- Splitter-UI/Views/MainWindow.axaml.cs
+++ Splitter-UI/Views/MainWindow.axaml.cs
@@ -1,3 +1,5 @@
+using Avalonia.Controls;
+
 namespace Splitter_UI.Views;
 
 public partial class MainWindow : Avalonia.Controls.Window
@@ -6,19 +8,5 @@ public partial class MainWindow : Avalonia.Controls.Window
     public MainWindow()
     {
         InitializeComponent();
-
-        //var uri = new Uri("avares://Splitter-UI/Assets/Fonts/Font Awesome 7 Free-Solid-900.otf");
-
-        //if (!AssetLoader.Exists(uri))
-        //{
-        //    Console.WriteLine("Resource NOT FOUND: " + uri);
-        //    return;
-        //}
-
-        //using var stream = AssetLoader.Open(uri);
-        //using var ms = new MemoryStream();
-        //stream.CopyTo(ms);
-
-        //Console.WriteLine("Resource FOUND. Size = " + ms.Length + " bytes");
     }
 }
